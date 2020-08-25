c109a897fe2b00bf192800adcfc3b618dc90ca0e
Brian Espinosa <b@bje.co>
2020-08-25 10:33:52 -0700

    Quick cleanup on package info for template alphas

--- templates/cra-template/template/src/App.test.js
+++ templates/cra-template/template/src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('app', () => {
+  it('renders learn react link', () => {
+    expect.assertions(1);
+
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+
+    expect(linkElement).toBeInTheDocument();
+  });
 });
