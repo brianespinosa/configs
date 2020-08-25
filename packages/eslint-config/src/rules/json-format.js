f1ddcce86e7b773aea88af7c22e4cdca824fcd27
Brian Espinosa <b@bje.co>
2020-08-24 21:52:08 -0700

    Fix the way package.json formatting was happening

--- packages/eslint-config/src/rules/json-format.js
+++ packages/eslint-config/src/rules/json-format.js
@@ -2,12 +2,13 @@
 
 module.exports = {
   plugins: ['json-format'],
-  settings: {
-    'json/sort-package-json': 'pro',
-    // NOTE: This 'json/ignore-files' configuration is not needed if we use ignorePatterns
-    // 'json/ignore-files': ['!**/package.json'],
-  },
-  // FIRST ignore all .json files
-  // THEN look only at package.json files (double negative)
-  ignorePatterns: ['**/*.json', '!**/package.json'],
+  // Eslint does not normally see .json files. This makes it see package.json files specifically with this rule
+  overrides: [
+    {
+      files: ['package.json'],
+      settings: {
+        'json/sort-package-json': 'pro',
+      },
+    },
+  ],
 };
